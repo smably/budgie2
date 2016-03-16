a9408976f9f4b0597ec27b3d29d159462c1d64a4
Sylvan Mably <smably@ebay.com>
2016-03-16 00:55:31 -0400

    Initial rrule proof of concept

--- components/app.js
+++ components/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RRule, RRuleSet, rrulestr } from '../lib/rrule/rrule';
 
 import accounts from '../data/accounts.json';
 import transactions from '../data/transactions.json';
@@ -8,6 +9,8 @@ import recurrences from '../data/recurrences.json';
 class App extends React.Component {
     componentDidMount() {
         console.log(accounts, transactions, recurrences);
+        let rule = rrulestr(recurrences[0].rruleset.join("\n"))
+        console.log(rule.all().map(date => date.toJSON()));
     }
 
     render() {
