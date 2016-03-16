720df094cec5eaf9307f28d0116e649435df3728
Sylvan Mably <smably@ebay.com>
2016-03-15 23:03:10 -0400

    Add some test data and json loader

--- components/app.js
+++ components/app.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import accounts from '../data/accounts.json';
+import transactions from '../data/transactions.json';
+import recurrences from '../data/recurrences.json';
+
 class App extends React.Component {
+    componentDidMount() {
+        console.log(accounts, transactions, recurrences);
+    }
+
     render() {
         return <h1>Budgie</h1>;
     }
--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,11 @@ module.exports = {
             query: {
                 presets: [ 'es2015', 'react' ]
             }
+        },
+        {
+            test: /.json$/,
+            loader: 'json'
         }
         ]
     }
 };
-
