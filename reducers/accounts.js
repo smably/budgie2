00346408d2c151228274f567ffe2cab16c258c3a
Sylvan Mably <smably@ebay.com>
2016-04-03 19:59:28 -0400

    Remove Redux dependency for now

--- reducers/accounts.js
+++
@@ -1,9 +0,0 @@
-const accounts = (state, action) => {
-    switch (action.type) {
-        case 'RECEIVE_ACCOUNTS':
-            return action.accounts;
-            break;
-        default:
-            return state;
-    }
-}
